a475a8a903a79b513517c5e97dccedb579e3f05b
Abbadon1334 <fdanti@gmail.com>
2020-05-18 16:32:00 +0200

    add some comments

--- xml/index.php
+++ xml/index.php
@@ -21,6 +21,8 @@ $dsn = 'mysql://127.0.0.1:3306/test';
 $usr = 'atk4_test';
 $pwd = 'atk4_pass';
 
+// define model : https://agile-data.readthedocs.io/en/develop/
+
 class Book extends Model
 {
     public $table = 'books';
@@ -38,45 +40,54 @@ class Book extends Model
     }
 }
 
-$xml = simplexml_load_file($file);
+
+$xml = simplexml_load_file($file); // read XML
 $ds = [];
-$array_persistence = new Array_($ds);
-$model = new Book($array_persistence);
+$array_persistence = new Array_($ds); // create model storage persistence
+$model = new Book($array_persistence); // create the mode with the persistence
 
 foreach ($xml->book as $element) {
-    $model->unload();
+    $model->unload(); // unload model id + data
     foreach ($element as $key => $val) {
-        $model->set($key, (string)$val);
+        $model->set($key, (string)$val); // set data fields
     }
-    $model->save();
+    $model->save(); // save the model to persistence
 }
 
-$app = new App(['title' => 'test xml']);
-$app->initLayout(Generic::class);
+$app = new App(['title' => 'test xml']); // create App with a title
+$app->initLayout(Generic::class); // initialize layout
 
-$menu = Menu::addTo($app)->addClass('inverted');
-$menu->addItem('Create Table in DB')
-    ->on('click', function ($v) use ($dsn, $usr, $pwd) {
-        $persistence = Persistence::connect($dsn, $usr, $pwd);
+$menu = Menu::addTo($app); // add a FomanticUI menu to app
+$menu->addClass('inverted'); // add a CSS class to menu
+$menu->addItem('Create Table in DB') // add menu item
+    ->on('click', function ($v) use ($dsn, $usr, $pwd) { // create callback
 
-        $msg = Message::addTo($v,['Migration']);
-        $msg->text = Migration::getMigration(new Book($persistence))->migrate();
+        // this part of the code will be called only on click of the button
 
-        return $msg;
+        $persistence = Persistence::connect($dsn, $usr, $pwd); // connect to db
+
+        $msg = Message::addTo($v,['Migration']); // create a window message
+        $msg->text = Migration::getMigration(new Book($persistence))->migrate(); // create Table
+
+        return $msg; // return the message to update the user
     });
 
 
 $menu->addItem('Populate DB Table')
     ->on('click', function ($v) use ($dsn, $usr, $pwd, $model) {
 
-        $persistence = Persistence::connect($dsn, $usr, $pwd);
-        $model_db = new Book($persistence);
-        foreach($model->getIterator() as $m) {
-            $model_db->tryLoad($m->id);
-            $model_db->save($m);
+        // this part of the code will be called only on click of the button
+
+        $persistence = Persistence::connect($dsn, $usr, $pwd); // connect to db
+        $model_db = new Book($persistence); // use persistence DB for Model
+        foreach($model->getIterator() as $m) { // iterate the model with persistence array
+            $model_db->tryLoad($m->id); // try to reload a previous inserted record
+            $model_db->save($m); // save model data
         }
 
-        return new jsToast('Successful Populate DB Table');
+        return new jsToast('Successful Populate DB Table'); // return feedback to user
     });
 
-Table::addTo($app)->addClass('tiny')->setModel($model);
+$table = Table::addTo($app); // add FomanticUI table to App
+$table->addClass('tiny'); // add a CSS class to Table
+$table->setModel($model); // define the model to be show in the table as the model with array persistence
