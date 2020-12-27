d9d485a92c9d7303e856db6a803e97a53ba286b6
Francesco Danti <fdanti@gmail.com>
2020-12-27 12:13:25 +0100

    refactor for atk develop

--- playground/xml/index.php
+++ playground/xml/index.php
@@ -2,24 +2,21 @@
 
 declare(strict_types=1);
 
-use atk4\data\Model;
-use atk4\data\Persistence;
-use atk4\data\Persistence\Array_;
-use atk4\schema\Migration;
-use atk4\ui\App;
-use atk4\ui\jsToast;
-use atk4\ui\Layout\Generic;
-use atk4\ui\Menu;
-use atk4\ui\Message;
-use atk4\ui\Table;
+use Atk4\Data\Model;
+use Atk4\Data\Persistence;
+use Atk4\Data\Persistence\Array_;
+use Atk4\Ui\App;
+use Atk4\Ui\jsToast;
+use Atk4\Ui\Layout\Generic;
+use Atk4\Ui\Menu;
+use Atk4\Ui\Message;
+use Atk4\Ui\Table;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__).'/../vendor/autoload.php';
 
 $file = 'file.xml';
 
-$dsn = 'mysql://127.0.0.1:3306/test';
-$usr = 'atk4_test';
-$pwd = 'atk4_pass';
+$dsn = 'sqlite::memory:';
 
 // define model : https://agile-data.readthedocs.io/en/develop/
 
@@ -27,7 +24,7 @@ class Book extends Model
 {
     public $table = 'books';
 
-    public function init(): void
+    protected function init(): void
     {
         parent::init();
 
@@ -46,23 +43,23 @@ $array_persistence = new Array_($ds); // create model storage persistence
 $model             = new Book($array_persistence); // create the mode with the persistence
 
 foreach ($xml->book as $element) {
-    $model->unload(); // unload model id + data
+    $bookModel = clone $model; // unload model id + data
     foreach ($element as $key => $val) {
-        $model->set($key, (string) $val); // set data fields
+        $bookModel->set($key, (string) $val); // set data fields
     }
-    $model->save(); // save the model to persistence
+    $bookModel->save(); // save the model to persistence
 }
 
 $app = new App(['title' => 'test xml']); // create App with a title
-$app->initLayout(Generic::class); // initialize layout
+$app->initLayout([\Atk4\Ui\Layout::class]); // initialize layout
 
 $menu = Menu::addTo($app); // add a FomanticUI menu to app
 $menu->addClass('inverted'); // add a CSS class to menu
 $menu->addItem('Create Table in DB') // add menu item
-    ->on('click', function ($v) use ($dsn, $usr, $pwd) { // create callback
+    ->on('click', function ($v) use ($dsn) { // create callback
         // this part of the code will be called only on click of the button
 
-        $persistence = Persistence::connect($dsn, $usr, $pwd); // connect to db
+        $persistence = Persistence::connect($dsn); // connect to db
 
         $msg = Message::addTo($v, ['Migration']); // create a window message
         $msg->text = Migration::getMigration(new Book($persistence))->migrate(); // create Table
@@ -71,10 +68,10 @@ $menu->addItem('Create Table in DB') // add menu item
     });
 
 $menu->addItem('Populate DB Table')
-    ->on('click', function ($v) use ($dsn, $usr, $pwd, $model) {
+    ->on('click', function ($v) use ($dsn, $model) {
         // this part of the code will be called only on click of the button
 
-        $persistence = Persistence::connect($dsn, $usr, $pwd); // connect to db
+        $persistence = Persistence::connect($dsn); // connect to db
         $model_db = new Book($persistence); // use persistence DB for Model
         foreach ($model->getIterator() as $m) { // iterate the model with persistence array
             $model_db->tryLoad($m->id); // try to reload a previous inserted record
